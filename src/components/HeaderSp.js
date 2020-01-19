d0aac48edcc2bd5f60340c4495821deb2ae0951d
marianalfr <marianalfr@gmail.com>
2020-01-20 00:49:30 +0100

    Add cv

--- src/components/HeaderSp.js
+++ src/components/HeaderSp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from './Logo';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Pdf from '../documents/CV-Mariana-Lerma.pdf';
 
 class HeaderSp extends React.Component {
     constructor(props) {
@@ -52,7 +53,8 @@ class HeaderSp extends React.Component {
                     <ul id="nav" className={`header__nav-menu ${!this.state.menuOpen ? '' : ' show-menu'}`}>
                         <Link to="/es/portfolio"><li id="portfolio" onClick={this.openSection}>Portfolio.</li></Link>
                         <Link to="/es/sobre-mi"><li id="about" onClick={this.openSection}>Sobre mí.</li></Link>
-                        <li id="skills" onClick={this.openSection}>Habilidades.</li>
+                        {/* <li id="skills" onClick={this.openSection}>Habilidades.</li> */}
+                        <a href = {Pdf} target = "_blank"><li id="skills" onClick={this.openSection}>CV.</li></a>
                         <Link to="/es/contacta"><li id="contact" onClick={this.openSection}>Contacto.</li></Link>
                     </ul>
                 </nav>
