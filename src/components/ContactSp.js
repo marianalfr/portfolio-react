2cdd7c5bfedac4a7e556a6ad701ef9bddbcf092a
marianalfr <marianalfr@gmail.com>
2020-01-20 00:26:25 +0100

    Add send form functionality

--- src/components/ContactSp.js
+++ src/components/ContactSp.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as emailjs from 'emailjs-com';
+import close from '../images/close.svg';
 
 class ContactSp extends React.Component{
     constructor(props){
@@ -6,14 +8,63 @@ class ContactSp extends React.Component{
         this.state = {
             name: '',
             email: '',
-            message: ''
+            message: '',
+            isSent: ''
         }
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.resetForm = this.resetForm.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
 
     handleChange(event){
         this.setState({
             [event.target.name] : event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const { name, email, message } = this.state;
+
+        let templateParams = {
+          user_name: name,
+          user_email: email,
+          message: message
+        };
+
+        emailjs.send(
+          'gmail',
+          'contact_form',
+           templateParams,
+          'user_lGsf2zDEqp30Y4HzUlYeH'
+        )
+        .then((result) => {
+            console.log(result.text);
+            this.setState({
+                isSent: true
+            })
+        }, (error) => {
+            console.log(error.text);
+            this.setState({
+                isSent: false
+            })
+        });
+
+        this.resetForm();
+    }
+
+    resetForm(){
+        this.setState({
+            name: '',
+            email: '',
+            message: ''
+        });
+    }
+
+    handleClose(){
+        this.setState({
+            isSent: ''
         })
     }
 
@@ -28,23 +79,33 @@ class ContactSp extends React.Component{
                     <a href="https://twitter.com/MaLerFer" target="_blank" rel="noopener noreferrer"><i className="footer-link fab fa-twitter"></i></a>
                 </div>
                 <p className="contact__text">Estoy accesible a través de mi correo electrónico, mis redes sociales, o de este formulario:</p>
-                <form className="contact__form">
+                <form className="contact__form" onSubmit={this.handleSubmit}>
                     <div className="contact__form-details">
                         <div className="contact__form-details-input">
                             <label htmlFor="name">Nombre:</label>
-                            <input type="text" id="name" name="name"></input>
+                            <input type="text" id="name" name="name" value={this.state.name} onChange={this.handleChange}></input>
                         </div>
                         <div className="contact__form-details-input">
                             <label htmlFor="email">E-mail:</label>
-                            <input type="text" id="email" name="email"></input>
+                            <input type="text" id="email" name="email" value={this.state.email} onChange={this.handleChange}></input>
                         </div>
                     </div>
                     <div className="contact__form-text">
                         <label htmlFor="textarea">Mensaje:</label>
-                        <textarea id="textarea" name="message"></textarea>
+                        <textarea id="textarea" name="message" value={this.state.message} onChange={this.handleChange}></textarea>
                     </div>
-                    <input type="submit" value="enviar"></input>
+                    <input className="btn-send" type="submit" value="enviar"></input>
                 </form>
+                <div className={`contact__modal ${this.state.isSent === true ? '' : ' hidden'}`}>
+                    <img src={close} alt="close" onClick={this.handleClose}/>
+                    <h3>¡Recibido!.</h3>
+                    <p>Gracias por ponerte en contacto conmigo, responderé lo antes posible.</p>
+                </div>
+                <div className={`contact__modal ${this.state.isSent === false ? '' : ' hidden'}`}>
+                    <img src={close} alt="close" onClick={this.handleClose}/>
+                    <h3>Ha habido un error.</h3>
+                    <p>Por favor, inténtalo de nuevo o utiliza otra de las vías de contacto.</p>
+                </div>
             </section>
         )
     }
