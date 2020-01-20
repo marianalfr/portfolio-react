98733e8aaac5b77159e33603148534f81ebff75d
marianalfr <marianalfr@gmail.com>
2020-01-20 01:01:22 +0100

    Create docs

---
+++ docs/precache-manifest.8f4afdab71cac747d5aaa1197ff4529a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388a989190b977de040b8ea62bebe948",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44264d42a48fc8459fab",
+    "url": "./static/css/main.3d895f45.chunk.css"
+  },
+  {
+    "revision": "cad8f9b3d036393ce8b7",
+    "url": "./static/js/2.62494a26.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.62494a26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44264d42a48fc8459fab",
+    "url": "./static/js/main.bc3282a1.chunk.js"
+  },
+  {
+    "revision": "99493b6f5bfcbcbfa9dd",
+    "url": "./static/js/runtime-main.59c71401.js"
+  },
+  {
+    "revision": "43c9f40feb340cfe08bf29c1f3d1a8ea",
+    "url": "./static/media/Awesome-profile-cards-Mariana-Lerma.43c9f40f.png"
+  },
+  {
+    "revision": "e9e6b782e6cb3c28f000d34a06400066",
+    "url": "./static/media/Bel-And-Mums-Mariana-Lerma.e9e6b782.png"
+  },
+  {
+    "revision": "91c3ad3a2a2cb83001cf7f169dc18c37",
+    "url": "./static/media/CV-Mariana-Lerma.91c3ad3a.pdf"
+  },
+  {
+    "revision": "0767452151f766c320cc3801382c14d3",
+    "url": "./static/media/JSA-Mariana-Lerma.07674521.png"
+  },
+  {
+    "revision": "5f049f12e82adb9a5f606c84b8519ccc",
+    "url": "./static/media/MB-Mariana-Lerma.5f049f12.png"
+  },
+  {
+    "revision": "e16c13d52d0a2d9a53c7ac1e0a8b0ab2",
+    "url": "./static/media/Micomoler-Mariana-Lerma.e16c13d5.png"
+  },
+  {
+    "revision": "6fb96229d0b9066f20f32621f5baaa1d",
+    "url": "./static/media/Series-Mariana-Lerma.6fb96229.png"
+  },
+  {
+    "revision": "5f9be498a980d0fc03fa7b2bd6528d4b",
+    "url": "./static/media/Skylabers-Mariana-Lerma.5f9be498.png"
+  },
+  {
+    "revision": "676d86ecd32399b31282c32c979b5ed4",
+    "url": "./static/media/Wedding-Site-Mariana-Lerma.676d86ec.png"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "./static/media/close.777b93c6.svg"
+  },
+  {
+    "revision": "16f59bfe367b7f67b1925f92e59aa1e4",
+    "url": "./static/media/m-l.16f59bfe.jpg"
+  }
+]);
